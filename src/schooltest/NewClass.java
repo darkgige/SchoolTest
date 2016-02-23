b80231c096fb53bb677b49ce7e55c182aca5e823
FSEVERI\reccanello2998 <FSEVERI\reccanello2998@lmatbi-06.fseveri.it>
2016-02-23 08:52:30 +0100

    initialized repository 2

--- src/schooltest/NewClass.java
+++ src/schooltest/NewClass.java
@@ -9,14 +9,6 @@ package schooltest;
  *
  * @author FSEVERI\reccanello2998
  */
-public class SchoolTest {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello world");
-    }
+public class NewClass {
     
 }
